18cc08d6c89e6fd6a77fe9accb46a1645d866dee
Altamas <altmasalam00@gmai.com>
2021-03-11 18:20:19 +0530

    Profile Updated

--- app/src/main/java/com/cheetah/setu/account/ProfileFragment.java
+++ app/src/main/java/com/cheetah/setu/account/ProfileFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.cheetah.setu.R;
 
@@ -16,6 +18,9 @@ import com.cheetah.setu.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    LinearLayout profile;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +66,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profile = view.findViewById(R.id.profile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getContext(), "Mass", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
